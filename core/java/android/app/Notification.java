3494a7caa7f62ec794c590cdfaaaf3621c2bcc28
Marko Man <darkobas@gmail.com>
2018-04-18 11:41:28 +0200

    themes: Add notification theming support
    credits to @ezio84, @darkobas and @maxwen
    
    squashed commits:
    
    * Expose notification info text color
    by @ezio84
    
    * Add support for theming notifications
    by @darkobas
    @maxwen Fix notification text when using dark themes
    
    * Fix notification overlay config
    by @maxwen
    for notification overlays to correctly tint notification texts
    we must add a way they can override whatever is in the primary
    overlay
    
    to make things easier we only use one for both
    config_useDarkBgNotificationIconTextTinting
    and config_enableNightMode since they are never
    used different anyway
    
    notification overlays need to use
    <bool name="config_notificationTinting_override">true</bool>
    and can then use config_useDarkBgNotificationTinting_override
    which will be used instead of both the other configs
    above
    
    * adapted to work with rr v6.0.0
    
    Change-Id: I79d5d2418ad19026b9b90908de4256823d240876
    Co-authored-by: ezio84 <brabus84@gmail.com>

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -2869,6 +2869,8 @@ public class Notification implements Parcelable
                 mInNightMode = (currentConfig.uiMode & Configuration.UI_MODE_NIGHT_MASK)
                         == Configuration.UI_MODE_NIGHT_YES;
             }
+            // UI_MODE_NIGHT doesnt seem to be ready, so just listen to config
+            mInNightMode = getColorUtil().getNightModeNotification(mContext);
 
             if (toAdopt == null) {
                 mN = new Notification();
@@ -4860,7 +4862,7 @@ public class Notification implements Parcelable
 
         private CharSequence processLegacyText(CharSequence charSequence, boolean ambient) {
             boolean isAlreadyLightText = isLegacy() || textColorsNeedInversion();
-            boolean wantLightText = ambient;
+            boolean wantLightText = ambient || getColorUtil().getDarkNotificationTinting(mContext);
             if (isAlreadyLightText != wantLightText) {
                 return getColorUtil().invertCharSequenceColors(charSequence);
             } else {
@@ -6305,7 +6307,7 @@ public class Notification implements Parcelable
                 sb.append(bidi.unicodeWrap(m.mSender),
                         makeFontColorSpan(colorize
                                 ? builder.getPrimaryTextColor()
-                                : builder.getSenderTextColor()),
+                                : mBuilder.resolveContrastColor()),
                         0 /* flags */);
             }
             CharSequence text = m.mText == null ? "" : m.mText;
